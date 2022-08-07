96c4ff026e1219bdc6d0ffb665420f29938f01c2
Ekaterina-Rudenko <katestasevich30@gmail.com>
2022-08-07 22:37:13 +0300

    scope, value

---
+++ spring_core _without_xml/src/main/java/by/springcourse/PopMusic.java
@@ -0,0 +1,20 @@
+package by.springcourse;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PopMusic implements Music{
+  private List<String> songs = new ArrayList<>();
+  {
+    songs.add("Under Pressure");
+    songs.add("Summer Moved On");
+    songs.add("All Good Things");
+  }
+  @Override
+  public List<String> getSongs() {
+    return songs;
+  }
+}
