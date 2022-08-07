900a09a14d5823c64a5e8cd96b1d03bd50fd76d1
Ekaterina-Rudenko <katestasevich30@gmail.com>
2022-08-08 02:02:00 +0300

    add first, second servlets

---
+++ spring_mvc_app1/src/main/java/by/springcourse/controllers/SecondController.java
@@ -0,0 +1,13 @@
+package by.springcourse.controllers;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+@GetMapping("/exit")
+  public String exit(){
+    return "second/exit";
+  }
+}
