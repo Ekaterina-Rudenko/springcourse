ba0bb877aa7fdab5a3c7219c21e7996002a9986f
Ekaterina-Rudenko <katestasevich30@gmail.com>
2022-08-11 01:48:25 +0300

    add people controller

---
+++ CRUD/src/main/java/by/rudenko/springcourse/dao/PersonDao.java
@@ -0,0 +1,26 @@
+package by.rudenko.springcourse.dao;
+
+import by.rudenko.springcourse.models.Person;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonDao {
+
+  private static int PEOPLE_COUNT;
+  private List<Person> people;
+  {
+    people = new ArrayList<>();
+    people.add(new Person(++PEOPLE_COUNT, "Tom"));
+    people.add(new Person(++PEOPLE_COUNT, "Bob"));
+    people.add(new Person(++PEOPLE_COUNT, "Mike"));
+    people.add(new Person(++PEOPLE_COUNT, "Alex"));
+  }
+  public List<Person> index(){
+    return people;
+  }
+  public Person show(int id){
+    return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+  }
+}
