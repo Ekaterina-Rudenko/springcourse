66b4e82ff8d5581f771f32ff0f18551c439f55d2
Ekaterina-Rudenko <katestasevich30@gmail.com>
2022-08-11 02:28:05 +0300

    add post new person

--- CRUD/src/main/java/by/rudenko/springcourse/controllers/PeopleController.java
+++ CRUD/src/main/java/by/rudenko/springcourse/controllers/PeopleController.java
@@ -1,11 +1,14 @@
 package by.rudenko.springcourse.controllers;
 
 import by.rudenko.springcourse.dao.PersonDao;
+import by.rudenko.springcourse.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -30,4 +33,17 @@ public class PeopleController {
     model.addAttribute("person", personDao.show(id));
     return "/people/show";
   }
+
+  @GetMapping("/new")
+  public String newPerson(@ModelAttribute("person") Person person /*Model model*/) {
+    /*model.addAttribute("person", new Person());*/ //annot ModelAttr can be used instead
+    return "/people/new";
+  }
+
+  @PostMapping()
+  public String create(@ModelAttribute("person") Person person) {
+    personDao.save(person);
+    return "redirect:/people";
+  }
+
 }
--- CRUD/src/main/java/by/rudenko/springcourse/models/Person.java
+++ CRUD/src/main/java/by/rudenko/springcourse/models/Person.java
@@ -9,6 +9,9 @@ private String name;
     this.name = name;
   }
 
+  public Person() {
+  }
+
   public int getId() {
     return id;
   }
